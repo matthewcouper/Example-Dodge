382470a84950b3ec9b6478904e86e13a3e528034
Matthew Couper <51890733+matthewcouper@users.noreply.github.com>
2020-06-29 10:50:00 +1200

    Planets and spaceships are drawn in panel

---
+++ Example Dodge/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Example_Dodge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrPlanet = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.Location = new System.Drawing.Point(0, 0);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(799, 453);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // TmrPlanet
+            // 
+            this.TmrPlanet.Enabled = true;
+            this.TmrPlanet.Tick += new System.EventHandler(this.TmrPlanet_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Timer TmrPlanet;
+    }
+}
+
---
+++ Example Dodge/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example_Dodge
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics g; //declare a graphics object called g
+        // declare space for an array of 7 objects called planet 
+        Planet[] planet = new Planet[7];
+        Spaceship spaceship = new Spaceship();
+        Random yspeed = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 7; i++)
+            {
+                int x = 10 + (i * 75);
+                planet[i] = new Planet(x);
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+         
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            for (int i = 0; i < 7; i++)
+            {
+                // generate a random number from 5 to 20 and put it in rndmspeed
+                int rndmspeed = yspeed.Next(5, 20);
+                planet[i].y += rndmspeed;
+
+
+                //call the Planet class's drawPlanet method to draw the images
+                planet[i].DrawPlanet(g);
+
+                spaceship.DrawSpaceship(g);
+
+            }
+  
+
+        }
+
+        private void TmrPlanet_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                planet[i].MovePlanet();
+                //if a planet reaches the bottom of the Game Area reposition it at the top
+                if (planet[i].y >= PnlGame.Height)
+                {
+                    planet[i].y = 30;
+                }
+
+            }
+
+            PnlGame.Invalidate();//makes the paint event fire to redraw the panel
+        }
+    }
+}
